8de38fd2b0c5c3c7e1342019bda2afc7de1ed5e9
fedekiwo <fedekiwo@gmail.com>
2021-03-01 20:31:49 -0300

    fix: empty string deault description

--- frontend/todo-list/src/components/md.js
+++ frontend/todo-list/src/components/md.js
@@ -4,7 +4,7 @@ const md = new MarkdownIt();
 
 export default function MarkDown(props) {
   const { description } = props
-  const markDown = md.render(description)
+  const markDown = md.render(description || "");
   return(
     <div
       style={{ border: "1px solid #767676", padding: "0 20px" }}
